a1e0ce43e45759654c170b38a6c6b6a1c1795e91
SychO9 <sychocouldy@gmail.com>
2021-02-23 16:08:45 +0100

    Add helper functions for clockwork laravel plugin

---
+++ src/helpers.php
@@ -0,0 +1,45 @@
+<?php
+
+use Flarum\Foundation\Paths;
+
+if (! function_exists('base_path')) {
+    /**
+     * Get the path to the base of the install.
+     *
+     * @param  string  $path
+     * @return string
+     * @deprecated Will be removed in Beta.15.
+     */
+    function base_path($path = '')
+    {
+        return app(Paths::class)->base.($path ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+}
+
+if (! function_exists('public_path')) {
+    /**
+     * Get the path to the public folder.
+     *
+     * @param  string  $path
+     * @return string
+     * @deprecated Will be removed in Beta.15.
+     */
+    function public_path($path = '')
+    {
+        return app(Paths::class)->public.($path ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+}
+
+if (! function_exists('storage_path')) {
+    /**
+     * Get the path to the storage folder.
+     *
+     * @param  string  $path
+     * @return string
+     * @deprecated Will be removed in Beta.15.
+     */
+    function storage_path($path = '')
+    {
+        return app(Paths::class)->storage.($path ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+}
